9fed9e48ff2ab546aad33b749264a02ef807270f
Jussi Utunen <jussi.utunen@gmail.com>
2020-03-10 13:39:16 +0200

    initial commit

---
+++ dist/minify.cmd
@@ -0,0 +1,1 @@
+terser custom-arrow.js -m --mangle-props reserved=[l,w,constructor,observedAttributes,disconnectedCallback,attributeChangedCallback,connectedCallback,Customarrow] -o custom-arrow.min.js
