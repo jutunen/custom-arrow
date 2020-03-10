cb84339f6e4f077b0f923d33b152b1d7eff8b88b
Jussi Utunen <jussi.utunen@gmail.com>
2020-03-10 17:04:38 +0200

    added node versions of the files

--- dist/minify.cmd
+++ dist/minify.cmd
@@ -1,1 +1,3 @@
 terser custom-arrow.js -m --mangle-props reserved=[l,w,constructor,observedAttributes,disconnectedCallback,attributeChangedCallback,connectedCallback,Customarrow] -o custom-arrow.min.js
+
+terser custom-arrow-node.js -m --mangle-props reserved=[l,w,constructor,observedAttributes,disconnectedCallback,attributeChangedCallback,connectedCallback,Customarrow] -o custom-arrow-node.min.js
